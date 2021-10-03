aab93e3c97688b9c14e59e5315cae81ca6dbb00d
Code Forget <srivastava998@gmail.com>
2021-10-03 17:07:04 -0400

    fix navigation

--- src/app/_modules/client/productlist/productlist.component.ts
+++ src/app/_modules/client/productlist/productlist.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ProductsService } from '../../../_core/_services/products.service';
 import { Product } from '../../../_core/_models/product';
-import {SelectItem} from 'primeng/api';
+import { SelectItem } from 'primeng/api';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
@@ -12,42 +12,45 @@ import { PrimeNGConfig } from 'primeng/api';
 export class ProductlistComponent implements OnInit {
   products: Product[];
 
-    sortOptions: SelectItem[];
+  sortOptions: SelectItem[];
 
-    sortOrder: number;
+  sortOrder: number;
 
-    sortField: string;
+  sortField: string;
 
   constructor(
-    private productService: ProductsService, 
+    private productService: ProductsService,
     private primengConfig: PrimeNGConfig) { }
 
   ngOnInit(): void {
-    debugger;
-    // this.productService.getProductsSmall().then(data => {
-		// 	this.products = data;
-    //   debugger;
-		// });
-    
-        this.sortOptions = [
-            {label: 'Price High to Low', value: '!price'},
-            {label: 'Price Low to High', value: 'price'}
-        ];
-        debugger;
-        this.primengConfig.ripple = true;
+    this.sortOptions = [
+      { label: 'Price High to Low', value: '!price' },
+      { label: 'Price Low to High', value: 'price' }
+    ];
+    this.primengConfig.ripple = true;
+
+    this.getProductList();
   }
 
   onSortChange(event) {
     let value = event.value;
 
     if (value.indexOf('!') === 0) {
-        this.sortOrder = -1;
-        this.sortField = value.substring(1, value.length);
+      this.sortOrder = -1;
+      this.sortField = value.substring(1, value.length);
     }
     else {
-        this.sortOrder = 1;
-        this.sortField = value;
+      this.sortOrder = 1;
+      this.sortField = value;
     }
-}
+  }
+
+  getProductList() {
+    this.productService.getProducts().subscribe(data => {
+      debugger;
+      console.log(data);
+      this.products = data;
+    });
+  }
 
 }
